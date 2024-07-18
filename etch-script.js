65e9d6a4a67e7b41ab899340225352210649c1d4
cgen101 <c_g638@txstate.edu>
2024-07-18 15:17:00 -0500

    layout continues

--- etch-script.js
+++ etch-script.js
@@ -2,19 +2,44 @@ const grid = document.getElementById("container");
 const slider = document.getElementById("myRange");
 const sliderVal = document.getElementById("slideValue");
 const clearButton = document.getElementById("delIcon");
+const expButton = document.getElementById("saveIcon");
 const chooseColor = document.getElementById("pickColor");
 const sideNav = document.getElementById("adjustments");
+const randomColor = document.getElementById("randColorButt");
+const backToBlack = document.getElementById("boringButt");
 let gridElems = document.getElementsByClassName("gridDiv");
 
+//Load initial 16x16 grid
 document.addEventListener('DOMContentLoaded', function() {
     createGrid(16); 
     sliderVal.textContent = `16 x 16`; 
 })
 
+//Change grid density
 slider.oninput = function() { 
     createGrid(slider.value); 
     sliderVal.textContent = `${slider.value} x ${slider.value}`; 
 };
+
+//Change color to desired 
+chooseColor.oninput = function() { 
+    listenForHover(chooseColor.value);
+};
+
+backToBlack.addEventListener("click", function() { 
+    listenForHover("#000");
+})
+
+
+//Disco mode 
+randomColor.addEventListener("click", function() { 
+    for (let i=0; i<gridElems.length; i++) { 
+        gridElems[i].addEventListener("mouseover", function() { 
+            gridElems[i].style.backgroundColor = getRandomColor();
+        })
+    }
+})
+
 
 //Helper fxn to generate grid
 function createGrid(x) { 
@@ -27,14 +52,14 @@ function createGrid(x) {
         grid.appendChild(newDiv); 
     }
     
-    listenForHover();
+    listenForHover("#000");
 }
 
 //Helper fxn to add listener to each div
-function listenForHover() { 
+function listenForHover(currColor) { 
     for (let i=0; i<gridElems.length; i++) { 
         gridElems[i].addEventListener("mouseover", function() { 
-            gridElems[i].style.backgroundColor = chooseColor.value;
+            gridElems[i].style.backgroundColor = currColor;
         })
     }
 }
@@ -42,9 +67,37 @@ function listenForHover() {
 //Fxn to clear grid
 clearButton.addEventListener("click", function() { 
     for (let i = 0; i < gridElems.length; i++) {
-        gridElems[i].style.backgroundColor = "white"; 
+        gridElems[i].style.backgroundColor = "#eeeeee"; 
     }
 })
+
+//Randomize color helper fxn
+function getRandomColor() {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+//Save sketch as .png
+expButton.addEventListener('click', function() {
+    html2canvas(document.getElementById('container')).then(canvas => {
+        let link = document.createElement('a');
+        link.download = 'etch-a-sketch.png';
+        link.href = canvas.toDataURL();
+        link.click();
+    });
+});
+
+jQuery(document).ready(function(){
+    $('h1').mousemove(function(e){
+      var rXP = (e.pageX - this.offsetLeft-$(this).width()/2);
+      var rYP = (e.pageY - this.offsetTop-$(this).height()/2);
+      $('h1').css('text-shadow', +rYP/10+'px '+rXP/80+'px rgba(227,6,19,.8), '+rYP/8+'px '+rXP/60+'px rgba(255,237,0,1), '+rXP/70+'px '+rYP/12+'px rgba(0,159,227,.7)');
+    });
+ });
 
 function openNav() { 
     sideNav.style.width="250px"; 
@@ -52,6 +105,8 @@ function openNav() {
 function closeNav() { 
     sideNav.style.width="0px"; 
 }
+
+
 
 
 
